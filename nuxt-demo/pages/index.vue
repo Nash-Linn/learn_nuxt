e207039c70f40894c5d16a5c22957fa9571b311f
linnc <nash0207@163.com>
2023-07-03 17:29:52 +0800

    update

---
+++ nuxt-demo/pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div>
+      please
+      <nuxt-link to="/">login</nuxt-link>
+    </div>
+    <div>
+      您已登录，可以访问我的页面
+      <nuxt-link to="/my">my</nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "IndexPage",
+};
+</script>
